b66d0204d0edf87f1e070d08aebce01cdeaa7957
Tarek <mdtareksolaiman16@gmail.com>
2022-10-26 22:55:04 +0600

    home design ready

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,19 +1,52 @@
 import React from "react";
+import Lottie from "lottie-react";
+import reader from "../imges/90714-online-learning.json";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-6xl"> home page</h1>
-      <div className="w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+      <div className="lg:flex md:flex justify-center items-center bg-blue-300">
+        <div className="text-white md:w-2/5 lg:w-2/5">
+          <div className="text-center md:text-justify lg:text-justify ">
+            <p className="lg:text-5xl text-2xl font-bold">
+              Improve Your
+              <br />
+              Learning Experience
+              <br />
+              Better Instantly
+            </p>
           </div>
+          <p className="text-lg font-semibold pt-3">
+            We have <span className="text-yellow-400">40k+</span> Online courses
+            & <span className="text-yellow-400">500k+</span> Online registered
+            student. Find your desired Courses from them.
+          </p>
+
+          <p className="text-lg font-semibold pt-3">
+            <span className="text-yellow-400">500k+</span>People already trusted
+            us.{" "}
+            <Link to="/courses" className="text-yellow-400">
+              View Courses{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 inline"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                />
+              </svg>
+            </Link>
+          </p>
+        </div>
+        <div className="lg:w-2/5">
+          <Lottie animationData={reader} loop={true} />;
         </div>
       </div>
     </div>
